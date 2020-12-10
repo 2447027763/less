ce9b4dc4cf2f29be5abf41324e28e9b93b05455d
qqq <2447027763@qq.com>
2020-12-10 19:13:36 +0800

    上传less代码

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import index from './less/style.less';
+// import less from './less/index.less';
+// import jp1 from './img/timg.jfif';
+// import jp2 from './img/xm6.jpg';
+// import jp3 from './img/xm5.jpg';
+// import indexScss from './sass/index.scss';
+// var img2 =document.createElement("img");
+// img2.src=jp2;
+// document.querySelector("div").appendChild(img2);
+
+// var img1 =document.createElement("img");
+// img1.src=jp1;
+// document.querySelector("div").appendChild(img1);
+
+// var img3 =document.createElement("img");
+// img3.src=jp3;
+// document.querySelector("div").appendChild(img3);
